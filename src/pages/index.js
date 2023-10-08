1bdedb4bd4dbc2ab88d15818c83d5b7744faf336
ICE0208 <doky3307@naver.com>
2023-10-09 02:27:19 +0900

    (#2.1) Fetching Data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,36 @@
 import Seo from "@/components/Seo";
+import { useEffect, useState } from "react";
+
+const API_KEY = "API_KEY";
 
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+  const fetchMovie = async () => {
+    const url =
+      "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${API_KEY}`,
+      },
+    };
+    const response = await fetch(url, options);
+    const { results } = await response.json();
+    setMovies(results);
+  };
+
+  useEffect(() => {
+    fetchMovie();
+  }, []);
+
   return (
     <div>
       <Seo title="Home" />
-      <h1>HOME</h1>
-      <style jsx>{`
-        h1 {
-          font-size: 48px;
-        }
-      `}</style>
+      {!movies.length && <h4>Loading...</h4>}
+      {movies.map((movie) => (
+        <div key={movie.id}>{movie.original_title}</div>
+      ))}
     </div>
   );
 }
