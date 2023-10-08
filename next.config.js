07ab300bef2b4394f26373f70b8f73b46c5f435f
ICE0208 <doky3307@naver.com>
2023-10-09 03:38:47 +0900

    (#2.2) Redirect and Rewrite

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+const API_KEY = process.env.API_KEY;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/contact",
+        destination: "/form",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=${API_KEY}`,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,9 @@
 import Seo from "@/components/Seo";
 import { useEffect, useState } from "react";
-
-const API_KEY = "API_KEY";
-
 export default function Home() {
   const [movies, setMovies] = useState([]);
   const fetchMovie = async () => {
-    const url =
-      "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
-    const options = {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        Authorization: `Bearer ${API_KEY}`,
-      },
-    };
-    const response = await fetch(url, options);
+    const response = await fetch("/api/movies");
     const { results } = await response.json();
     setMovies(results);
   };
@@ -25,12 +13,39 @@ export default function Home() {
   }, []);
 
   return (
-    <div>
+    <div className="container">
       <Seo title="Home" />
       {!movies.length && <h4>Loading...</h4>}
       {movies.map((movie) => (
-        <div key={movie.id}>{movie.original_title}</div>
+        <div
+          className="movie"
+          key={movie.id}
+        >
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <h4>{movie.original_title}</h4>
+        </div>
       ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
 }
